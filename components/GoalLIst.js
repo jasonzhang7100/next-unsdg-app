f90a19b515cd61796e425777bd8400a2889065aa
Jason Zhang <jasonzhang.syd@gmail.com>
2021-07-07 22:21:08 +1000

    add getStaticProps test and adjust to through it

--- components/GoalLIst.js
+++ components/GoalLIst.js
@@ -2,7 +2,7 @@ import GoalItem from './GoalItem';
 
 const GoalLIst = ({ goals }) => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-6 mx-4 mt-6">
+    <div data-cy="goal-list" className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-6 mx-4 mt-6">
       {goals.map((goal) => (
         <GoalItem key={goal.code} goal={goal} />
       ))}
--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -9,6 +9,10 @@ context('Home Page Test', () => {
     cy.get('h1').contains('17 GOALS');
   });
 
+  it('should get goal list through getStaticProps', () => {
+    cy.get('[data-cy=goal-list]').should('have.length', 1).and('be.visible');
+  });
+
   it('should navigate to About Us page', () => {
     cy.get('[data-cy=nav-link-about]').contains('About').click();
     cy.url().should('include', '/about');
