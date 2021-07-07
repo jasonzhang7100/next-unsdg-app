44ddd25b88439ea3b1a3981c6a4d5be2cd292926
Jason Zhang <jasonzhang.syd@gmail.com>
2021-07-07 21:47:30 +1000

    fetch data through getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div>hello next</div>;
+export default function Home({ goals }) {
+  return (
+    <div>
+      <h1>hello next</h1>
+      {goals.map((goal) => (
+        <h2 key={goal.code}>{goal.title}</h2>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    'https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=false',
+  );
+  const goals = await res.json();
+  console.log(goals);
+  return {
+    props: {
+      goals,
+    },
+  };
+};
