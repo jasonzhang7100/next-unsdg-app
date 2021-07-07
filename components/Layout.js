17b3a0496e3adacb086642486c4c9374dfa07538
Jason Zhang <jasonzhang.syd@gmail.com>
2021-07-07 21:27:43 +1000

    add Meta and add Nav test

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,9 @@
+import Meta from './Meta';
+
 const Layout = ({ children }) => {
   return (
     <div>
+      <Meta />
       <main className="mx-auto max-w-screen-2xl min-w-360 pt-12 pb-4">
         {children}
       </main>
