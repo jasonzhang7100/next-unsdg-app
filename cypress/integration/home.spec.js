5904dd5f569bb42829fea57213b66cd075562dfe
Jason Zhang <jasonzhang.syd@gmail.com>
2021-07-07 22:09:34 +1000

    adjust Home by Home content test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,8 +1,12 @@
 /// <reference types = "cypress" />
 
 context('Home Page Test', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('http://localhost:3000');
+  });
+
+  it('should find the home page tags and relevant contents', () => {
+    cy.get('h1').contains('17 GOALS');
   });
 
   it('should navigate to About Us page', () => {
